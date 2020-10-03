f1fa9f48131a4e32c86498528194cb6bac3b2e3c
unknown <fankui_kong@163.com>
2020-10-03 12:17:37 +0800

    搭建框架

---
+++ src/main/java/com/leet/code/leetcode/LeetCodeApplication.java
@@ -0,0 +1,16 @@
+package com.leet.code.leetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fankui.kong
+ */
+@SpringBootApplication
+public class LeetCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetCodeApplication.class, args);
+    }
+
+}
